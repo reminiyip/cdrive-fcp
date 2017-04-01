45d9546c8de6d31ee2caf5b9a4c4f1fa0d0a5ad8
Rachel P.Y. Liao <u3512485@connect.hku.hk>
2017-04-02 01:25:36 +0800

    add media url

--- cdrive_fcp/urls.py
+++ cdrive_fcp/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.views import static
 
 from core import views as core_view
 
@@ -23,4 +25,6 @@ urlpatterns = [
     url(r'^', include('core.urls')),
     url(r'^', include('game.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': settings.MEDIA_ROOT}),
 ]
+
